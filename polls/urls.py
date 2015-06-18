6d9f1e625a2fff8a9130153e469195b1d17ab3df
Marloes de Krom <marloes.de.krom@gmail.com>
2015-06-18 13:48:20 +0200

    Corrected some errors

--- polls/urls.py
+++ polls/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     # ex: /polls/5/results/
     url(r'^(?P<pk>[0-9]+)/results/$', views.ResultsView.as_view(), name='results'),
     # ex: /polls/5/vote/
-    url(r'^(?P<pk>[0-9]+)/vote/$', views.vote, name='vote'),
+    url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
 
 
 ]
--- polls/views.py
+++ polls/views.py
@@ -20,17 +20,17 @@ class DetailView(generic.DetailView):
 
 class ResultsView(generic.DetailView):
     model = Question
-    template_name = 'polls/results.html'    
+    template_name = 'polls/results.html'
 
 def vote(request,question_id):
     p = get_object_or_404(Question, pk=question_id)
     try:
-        selected_choice = p.choice_set.get(pk=request.POST[choice])
+        selected_choice = p.choice_set.get(pk=request.POST['choice'])
     except (KeyError, Choice.DoesNotExist):
         #redisplay the question voting form
         return render (request, 'polls/detail.html', {'question': p, 'error_message': "You didn't select a choice."})
     else:
-        selected_choice.voytes +=1
+        selected_choice.votes +=1
         selected_choice.save()
         #return a HttpResponseRedirect after dealing with POST data
         #This prevents data from being posted twice
